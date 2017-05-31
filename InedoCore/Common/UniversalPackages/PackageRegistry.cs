22b13012079bee481c4e52c5072c9163098769f2
Greg Divis <gdivis@inedo.com>
2017-05-31 17:15:04 -0400

    removed using

--- InedoCore/Common/UniversalPackages/PackageRegistry.cs
+++ InedoCore/Common/UniversalPackages/PackageRegistry.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Runtime.Serialization.Json;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
