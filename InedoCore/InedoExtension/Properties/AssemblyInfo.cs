22d8a42bb35360e35ca209584b88383961962d47
Ben Lubar <ben.lubar@gmail.com>
2018-05-11 17:26:46 -0500

    Add missing AssemblyInfo attributes.

--- InedoCore/InedoExtension/Properties/AssemblyInfo.cs
+++ InedoCore/InedoExtension/Properties/AssemblyInfo.cs
@@ -1,3 +1,10 @@
 ﻿using System.Reflection;
+using System.Runtime.InteropServices;
 
-[assembly: AssemblyProduct("any")]
+[assembly: AssemblyTitle("InedoCore")]
+[assembly: AssemblyDescription("Contains core functionality for Inedo products.")]
+[assembly: AssemblyCompany("Inedo")]
+[assembly: AssemblyCopyright("Copyright © Inedo 2018")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.6")]
+[assembly: AssemblyFileVersion("1.0.6")]
