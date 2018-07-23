d6f6dd32493a242246d2c89c9feafa1676e8f33d
Ben Lubar <ben.lubar@gmail.com>
2018-07-23 11:37:59 -0500

    Synchronize ProGet registry operations so only one lock is acquired at a time.

--- InedoCore/InedoExtension/Operations/ProGet/PackageDeployer.cs
+++ InedoCore/InedoExtension/Operations/ProGet/PackageDeployer.cs
@@ -16,6 +16,8 @@ namespace Inedo.Extensions.Operations.ProGet
 {
     internal static class PackageDeployer
     {
+        private static readonly SemaphoreSlim registryLock = new SemaphoreSlim(1, 1);
+
         public static async Task DeployAsync(IOperationExecutionContext context, IProGetPackageInstallTemplate template, ILogSink log, string installationReason, bool recordDeployment, Action<OperationProgress> setProgress = null)
         {
             var fileOps = await context.Agent.GetServiceAsync<IFileOperationsExecuter>().ConfigureAwait(false);
@@ -213,6 +215,7 @@ namespace Inedo.Extensions.Operations.ProGet
                         InstalledUsing = $"{SDK.ProductName}/{SDK.ProductVersion} (InedoCore/{Extension.Version})"
                     };
 
+                    await registryLock.WaitAsync(context.CancellationToken).ConfigureAwait(false);
                     try
                     {
                         using (var cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(10)))
@@ -229,6 +232,10 @@ namespace Inedo.Extensions.Operations.ProGet
                     catch (TaskCanceledException)
                     {
                         log.LogWarning("Registering the package in the machine package registry timed out.");
+                    }
+                    finally
+                    {
+                        registryLock.Release();
                     }
                 }
             }
