6195926d245d61b0ff599bf48d41607f00067abe
Greg D <gdivis@inedo.com>
2019-09-19 16:28:08 -0400

    added AppliesToAttribute

--- InedoCore/InedoExtension/Properties/AssemblyInfo.cs
+++ InedoCore/InedoExtension/Properties/AssemblyInfo.cs
@@ -1,10 +1,12 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
+using Inedo.Extensibility;
 
 [assembly: AssemblyTitle("InedoCore")]
 [assembly: AssemblyDescription("Contains core functionality for Inedo products.")]
 [assembly: AssemblyCompany("Inedo")]
-[assembly: AssemblyCopyright("Copyright © Inedo 2018")]
+[assembly: AssemblyCopyright("Copyright © Inedo 2019")]
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("1.0.0")]
 [assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AppliesTo(InedoProduct.BuildMaster | InedoProduct.Otter | InedoProduct.ProGet)]
